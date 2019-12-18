dac72c4843548efe38eaacf38d38d8101412cfc2
Andrea Zambon <azambon@webgriffe.com>
2019-12-18 17:39:31 +0100

    Filters out some operations that cause problems when DB isolation is active

--- src/app/code/community/Webgriffe/DbIsolation/Model/Db/Adapter/Pdo/Mysql.php
+++ src/app/code/community/Webgriffe/DbIsolation/Model/Db/Adapter/Pdo/Mysql.php
@@ -25,4 +25,32 @@ class Webgriffe_DbIsolation_Model_Db_Adapter_Pdo_Mysql extends Magento_Db_Adapte
     {
         return $this->transactionLevelOffset;
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function disableTableKeys($tableName, $schemaName = null)
+    {
+        if (parent::getTransactionLevel() === 0) {
+            //Only do this when there is really no transaction open. Otherwise the ALTER TABLE operation of the parent
+            //method would kill the transaction
+            return parent::disableTableKeys($tableName, $schemaName);
+        }
+
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function enableTableKeys($tableName, $schemaName = null)
+    {
+        if (parent::getTransactionLevel() === 0) {
+            //Only do this when there is really no transaction open. Otherwise the ALTER TABLE operation of the parent
+            //method would kill the transaction
+            return parent::enableTableKeys($tableName, $schemaName);
+        }
+
+        return $this;
+    }
 }
